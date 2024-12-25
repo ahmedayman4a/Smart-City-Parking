b0e9b2c7fff52bdcb8d4de86d3b9b6b916876bb7
ebrahimAlaaeldin <ebrahim.alaa2627@yahoo.com>
2024-12-25 03:00:24 +0200

    first commit

---
+++ src/main/java/com/example/smart_city_parking_backend/SmartCityParkingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_city_parking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCityParkingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCityParkingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_city_parking_backend/SmartCityParkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_city_parking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityParkingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
